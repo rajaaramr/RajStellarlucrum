c5f6fdf83a74e72c31e6ecfac9ad3ebb5cffa9dc
rajaaramr <rajaaram@gmail.com>
2025-07-26 00:09:49 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("RajTask 7 – Option Analytics HUD")
+
+st.write("Click the button to fetch CE/PE and IV from Zerodha (function coming next).")
+
+if st.button("🔁 Fetch Option Data"):
+    st.success("Data fetch will be wired up soon.")
